bbb723cea18661a9fc3d7626e6d734f4a25da182
David Mostert <david.mostert@workforce.co.za>
2025-01-21 16:12:55 +0200

    Https added

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Diagnostics;
+
+namespace API.DTOs;
+
+public class UserDto
+{
+    public required string Username {get; set;}
+    public required string Token {get; set;}
+}
