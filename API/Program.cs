fd6d71bdd08a9ee6fb2cfe8a51aa614749f47d02
David Mostert <david.mostert@workforce.co.za>
2025-06-23 10:50:13 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,14 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
-app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,19 +3,28 @@ import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-  //title = 'DatingApp';
+    //title = 'DatingApp';
+  hhtp=inject(HttpClient);
+  users: any;
+
   private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.setCurrentUser();
+    this.hhtp.get('https://localhost:5001/api/users').subscribe({
+      next: reponse => this.users = reponse,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    })
   }
 
   setCurrentUser(){
